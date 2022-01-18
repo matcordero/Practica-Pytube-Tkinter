755e3a6f2634909a6770fa28a30e91e1e3acdde0
Matias Cordero <corderomatias2013@gmail.com>
2022-01-18 03:51:12 -0300

    problema con libreria

---
+++ main (2).py
@@ -0,0 +1,140 @@
+from tkinter import Grid, StringVar, Tk,Frame,Button,messagebox,Label,Entry
+from pytube import exceptions,YouTube
+import urllib.request
+from PIL import Image, ImageTk
+
+#Variables Globales
+global colorOscuro
+colorOscuro="#181818"
+
+class VentanaLink(Frame):
+    def __init__(self,master=None) -> None:
+        super().__init__(master, bg="#223eba")
+        self.master
+        self.pack()
+        self.view()
+    
+    def view(self):
+        link=StringVar()
+        #----------------------------
+        self.f1=Frame(self,bg="#223eba")
+        self.f1.pack()
+
+        lblLink=Label(self.f1,text="Ingresar Link del Video:",width=30,height=1,font=(10),fg="white",bg="#223eba")
+        lblLink.pack(pady=5)
+
+        self.txtLink=Entry(self.f1,width=45,textvariable=link)
+        self.txtLink.pack(side="bottom",padx=5,pady=5)
+        #----------------------------
+        self.f2=Frame(self,bg="#223eba")
+        self.f2.pack()
+        btnDescarga=Button(self.f2,text="Descargar",width=20,height=2,command=lambda:self.Descarga(link.get()),bg="#22c7ba")
+        btnDescarga.pack(side="right",pady=10,padx=5)
+
+        Button(self.f2,text="Cancelar",width=15,height=2,command=self.master.destroy,bg="#22c7ba").pack(side="left",pady=10,padx=5)
+        
+    def Descarga(self,url):
+        if self.Comprobar(url):
+            root2=Tk()
+            root2.geometry("1000x200")
+            root2.title("Ventana Video")
+            ventana=VentantaVideoInformacion(root2,url)
+            ventana.mainloop()
+
+    def Comprobar(self,url=""):
+        resultado=True
+        try:
+            video=YouTube(url)
+            video.check_availability()
+            if(video.age_restricted):
+                messagebox.showerror(title="Restriccion de Edad",message="El video no esta disponible para descarga")
+                resultado=False
+        except(exceptions.VideoUnavailable, exceptions.RegexMatchError):
+            messagebox.showerror(title="Error de Link",message="El link ingresado es Invalido")
+            resultado=False
+        except:
+            messagebox.showerror(title="Error de Video",message="Error Desconocido")
+            resultado=False
+        finally:
+            return resultado
+
+class VentantaVideoInformacion(Frame):
+    def __init__(self,master=None,link="") -> None:
+        super().__init__(master,bg=colorOscuro)
+        self.master=master
+        self.video=YouTube(link)
+        self.pack(expand=True,fill="both")
+        self.view()
+    
+    def view(self):
+        #Frame 1 Imagen
+        self.f1=Frame(self,bg=colorOscuro)
+        self.f1.grid(row=0,column=0,columnspan=2)
+        self.mostrar_imagen()
+        #Frame 2 Descripcion
+        self.f2=Frame(self,bg=colorOscuro)
+        self.f2.grid(row=0,column=2)
+        self.mostrar_Informacion()
+        #Columna Boton Descargar y Cancelar
+        self.f3=Frame(self,bg=colorOscuro)
+        self.f3.grid(row=0,column=3)
+        self.mostrar_descarga()
+        Grid.rowconfigure(self, 0, weight=1)
+        for y in range(1,4):
+            Grid.columnconfigure(self, y, weight=1)
+
+    def mostrar_imagen(self):
+        file="miniatura.jpg"
+        link_jpg=self.video.thumbnail_url
+        r=urllib.request.urlopen(link_jpg)
+        f=open(file,"wb")
+        f.write(r.read())
+        f.close()
+        load = Image.open(r"C:\\Users\\Mati\\Desktop\\Practica Python\\miniatura.jpg")
+        load = load.resize((300,200), Image.ANTIALIAS)
+        render = ImageTk.PhotoImage(load,master=self.f1)
+        img = Label(self.f1,image=render)
+        img.image = render
+        img.pack(expand=True,fill="both")
+    
+    def mostrar_Informacion(self):
+        lblTitulo=Label(self.f2,text=self.video.title,font=10,fg="white",bg=colorOscuro)
+        lblTitulo.grid(row=0,column=0)
+        lblAutor=Label(self.f2,text=self.video.author,font=10,fg="white",bg=colorOscuro)
+        lblAutor.grid(row=1,column=0)
+        lblDuracion=Label(self.f2,text="Duracion: " + str(self.video.length/60) + " minutos",font=10,fg="white",bg=colorOscuro)
+        lblDuracion.grid(row=2,column=0)
+        for x in range(1,3):
+            Grid.rowconfigure(self.f2, x, weight=1)
+        Grid.columnconfigure(self.f2, 0, weight=1)
+
+    def mostrar_descarga(self):
+        self.lblEstado=Label(self.f3,text="Estado: No descargado",font=10,fg="white",bg=colorOscuro)
+        self.lblEstado.pack(side="bottom",pady=10)
+        btnDescargar=Button(self.f3,text="Descargar",width=20,height=4,font=1,fg="white",bg="#171131",command=lambda:self.descargar_Video())
+        btnDescargar.pack(side="top",pady=10)
+
+    def descargar_Video(self):
+        valor=True
+        try:
+            self.video.streams.get_by_resolution("720p").download(fr"D:\de mati en disco d\Listas de Youtube\Videos Individuales")
+        except:
+            try:
+                self.video.streams.get_by_resolution("360p").download(fr"D:\de mati en disco d\Listas de Youtube\Videos Individuales") 
+            except:
+                messagebox.showerror(title="Error al descargar",text="Un error inesperado no permite la descarga")
+                valor=False
+                  
+        if valor:
+            self.lblEstado.config(text="Estado: Descargado")   
+        else:
+            self.lblEstado.config(text="Estado: Error al Descargar")
+
+def main():
+    root=Tk()
+    root.title("Programa de descarga video de Youtube")
+    ventana=VentanaLink(root)
+    ventana.mainloop()
+
+if __name__=="__main__":
+    main()
--- main.py
+++ main.py
@@ -1,15 +1,52 @@
-from tkinter import *
+from re import S
+from tkinter import Tk,Label,Button,Frame
 
-def cambiar_panel(master):
+
+"""def cambiar_panel(master):
     f1=Frame(master,width=400,height=400,bg="white")
     f1.grid(row=0,column=0)
 
 def botonFrame(master=None):
     boton=Button(master,text="tocar",command=lambda:cambiar_panel(frame1),bg="red")
-    boton.pack()
+    boton.pack()"""
 
-root=Tk()
-frame1=Frame(root,width=800,height=800,bg="orange")
+class ventana1(Frame):
+    def __init__(self,master=None):
+        super().__init__(master,width=800,height=800,bg="orange")
+        self.master=master
+        self.pack(expand=True,fill="both")
+        self.views()
+    def views(self):
+        self.f1=Frame(self,height=400,width=400,bg="red")
+        self.f1.grid(row=0,column=0)
+
+        self.f2=Frame(self,height=400,width=400,bg="green")
+        self.f2.grid(row=0,column=1)
+
+        self.elementos(self.f2)
+
+        self.f3=Frame(self,height=400,width=400,bg="blue")
+        self.f3.grid(row=1,column=0)
+
+        self.f4=Frame(self,height=400,width=400,bg="black")
+        self.f4.grid(row=1,column=1)
+
+        for x in range(2):
+            self.columnconfigure(x,weight=1)
+            self.rowconfigure(x,weight=1)
+    def elementos(self,master):
+        self.boton=Button(master,text="tocar",height=3,width=20,command=lambda:self.cambiar_blanco())
+        self.boton.pack()
+    def cambiar_blanco(self):
+        self.f5=Frame(self,height=400,width=400,bg="white")
+        self.f5.grid(row=0,column=0)
+
+if __name__=="__main__":
+    root=Tk()
+    ventana=ventana1(root)
+    ventana.mainloop()
+    
+"""frame1=Frame(root,width=800,height=800,bg="orange")
 frame1.pack()
 
 f1=Frame(frame1,width=400,height=400,bg="green")
@@ -23,9 +60,9 @@ f3=Frame(frame1,width=400,height=400,bg="blue")
 f3.grid(row=1,column=0)
 
 f4=Frame(frame1,width=400,height=400,bg="black")
-f4.grid(row=1,column=1)
+f4.grid(row=1,column=1)"""
 
 """"""
 
 
-frame1.mainloop()
+"""frame1.mainloop()"""
